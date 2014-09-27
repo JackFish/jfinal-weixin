3ede1819c354f5d308b8b6cdf242381c5085177f
James <jfinal@126.com>
2014-09-27 21:40:15 +0800

    jfinal weixin sdk alpha version commit

--- src/com/jfinal/weixin/demo/DemoController.java
+++ src/com/jfinal/weixin/demo/DemoController.java
@@ -7,118 +7,172 @@
 package com.jfinal.weixin.demo;
 
 import com.jfinal.weixin.sdk.jfinal.WeixinController;
-import com.jfinal.weixin.sdk.message.in.InImageMessage;
-import com.jfinal.weixin.sdk.message.in.InLinkMessage;
-import com.jfinal.weixin.sdk.message.in.InLocationMessage;
-import com.jfinal.weixin.sdk.message.in.InMessage;
-import com.jfinal.weixin.sdk.message.in.InTextMessage;
-import com.jfinal.weixin.sdk.message.in.InVideoMessage;
-import com.jfinal.weixin.sdk.message.in.InVoiceMessage;
-import com.jfinal.weixin.sdk.message.in.event.InFollowEvent;
-import com.jfinal.weixin.sdk.message.in.event.InLocationEvent;
-import com.jfinal.weixin.sdk.message.in.event.InMenuEvent;
-import com.jfinal.weixin.sdk.message.in.event.InQrCodeEvent;
-import com.jfinal.weixin.sdk.message.in.speech_recognition.InSpeechRecognitionResults;
-import com.jfinal.weixin.sdk.message.out.OutMessage;
-import com.jfinal.weixin.sdk.message.out.OutTextMessage;
+import com.jfinal.weixin.sdk.msg.in.InImageMsg;
+import com.jfinal.weixin.sdk.msg.in.InLinkMsg;
+import com.jfinal.weixin.sdk.msg.in.InLocationMsg;
+import com.jfinal.weixin.sdk.msg.in.InTextMsg;
+import com.jfinal.weixin.sdk.msg.in.InVideoMsg;
+import com.jfinal.weixin.sdk.msg.in.InVoiceMsg;
+import com.jfinal.weixin.sdk.msg.in.event.InFollowEvent;
+import com.jfinal.weixin.sdk.msg.in.event.InLocationEvent;
+import com.jfinal.weixin.sdk.msg.in.event.InMenuEvent;
+import com.jfinal.weixin.sdk.msg.in.event.InQrCodeEvent;
+import com.jfinal.weixin.sdk.msg.in.speech_recognition.InSpeechRecognitionResults;
+import com.jfinal.weixin.sdk.msg.out.OutMusicMsg;
+import com.jfinal.weixin.sdk.msg.out.OutNewsMsg;
+import com.jfinal.weixin.sdk.msg.out.OutTextMsg;
+import com.jfinal.weixin.sdk.msg.out.OutVideoMsg;
+import com.jfinal.weixin.sdk.msg.out.OutVoiceMsg;
 
 /**
- * 设置好 url 与 token 即可开始开发
+ * 设置好weixin开发者中心的 URL 与 token 即可开始开发，并将此 DemoController 的
+ * index 方法映射到开发者中心的 URL 之上即可用个人微信测试效果，以实现极速微信开发
  */
 public class DemoController extends WeixinController {
 	
+	/*
+	 * weixin 公众号服务器调用唯一入口，即在开发者中心输入的 url 必须要指向此 action(non-Javadoc)
+	 * 必须要调用父类的 index() 方法，否则无法进行消息的解析与调度
+	 * 可以省图此方法，因为父类 WeixinController 中的 index 会被继承过来
+	 */
 	public void index() {
-		System.out.println(getInMessageXml());
-		super.index();
-		// String inMessageXml = getInMessageXml();
-		// 可将 inMessageXml 写入数据库以便查看
-		// new InMessave(inMessageXml).save();
-	}
-	
-	private void initTestOutTextMessage(OutMessage outMsg, InMessage inMsg) {
-		outMsg.setToUserName(inMsg.getFromUserName());
-		outMsg.setFromUserName(inMsg.getToUserName());
-		outMsg.setCreateTime(outMsg.now());
-		outMsg.setMsgType("text");
-		render(outMsg);
-	}
-	
-	protected void processInTextMessage(InTextMessage inTextMessage) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inTextMessage);
-		outMsg.setContent("processInTextMessage() 方法测试成功");
+		super.index();	// 必须要高度用父类的 index() 方法
 		
-		// 可以 render 各种类型的消息
-		// 具体看 com.jfinal.weixin.sdk.message.out 包下面的各类消息类
+		// String inMsgXml = getInMsgXml();
+		// 可将 inMsgXml 写入数据库以便查看
+		// new InMsg(inMsgXml).save();
+	}
+	
+	/**
+	 * 实现父类抽方法，处理文本消息
+	 * 本例子中根据消息中的不同文本内容分别做出了不同的响应，同时也是为了测试 jfinal weixin sdk的基本功能：
+	 *     本方法仅测试了 OutTextMsg、OutNewsMsg、OutMusicMsg 三种类型的OutMsg，
+	 *     其它类型的消息会在随后的方法中进行测试
+	 */
+	protected void processInTextMsg(InTextMsg inTextMsg) {
+		String msgContent = inTextMsg.getContent().trim();
+		// 帮助提示
+		if ("help".equalsIgnoreCase(msgContent)) {
+			OutTextMsg outMsg = new OutTextMsg(inTextMsg);
+			outMsg.setContent("发送 help 可获得帮助，发送 news 可收取图文消息，发送 music 可获取音乐消息");
+			render(outMsg);
+		}
+		// 图文消息测试
+		else if ("news".equalsIgnoreCase(msgContent)) {
+			OutNewsMsg outMsg = new OutNewsMsg(inTextMsg);
+			outMsg.addNews("JFinal 1.8 发布，JAVA 极速 WEB+ORM 框架", "现在就加入 JFinal 极速开发世界，节省更多时间去跟女友游山玩水 ^_^", "http://mmbiz.qpic.cn/mmbiz/zz3Q6WSrzq1ibBkhSA1BibMuMxLuHIvUfiaGsK7CC4kIzeh178IYSHbYQ5eg9tVxgEcbegAu22Qhwgl5IhZFWWXUw/0", "http://mp.weixin.qq.com/s?__biz=MjM5ODAwOTU3Mg==&mid=200313981&idx=1&sn=3bc5547ba4beae12a3e8762ababc8175#rd");
+			outMsg.addNews("JFinal 1.6 发布,JAVA极速WEB+ORM框架", "JFinal 1.6 主要升级了 ActiveRecord 插件，本次升级全面支持多数源、多方言、多缓", "http://mmbiz.qpic.cn/mmbiz/zz3Q6WSrzq0fcR8VmNCgugHXv7gVlxI6w95RBlKLdKUTjhOZIHGSWsGvjvHqnBnjIWHsicfcXmXlwOWE6sb39kA/0", "http://mp.weixin.qq.com/s?__biz=MjM5ODAwOTU3Mg==&mid=200121522&idx=1&sn=ee24f352e299b2859673b26ffa4a81f6#rd");
+			render(outMsg);
+		}
+		// 音乐消息测试
+		else if ("music".equalsIgnoreCase(msgContent)) {
+			OutMusicMsg outMsg = new OutMusicMsg(inTextMsg);
+			outMsg.setTitle("Listen To Your Heart");
+			outMsg.setDescription("建议在 WIFI 环境下流畅欣赏此音乐");
+			outMsg.setMusicUrl("http://www.jfinal.com/Listen_To_Your_Heart.mp3");
+			outMsg.setHqMusicUrl("http://www.jfinal.com/Listen_To_Your_Heart.mp3");
+			outMsg.setFuncFlag(true);
+			render(outMsg);
+		}
+		// 其它文本消息直接返回原值
+		else {
+			OutTextMsg outMsg = new OutTextMsg(inTextMsg);
+			outMsg.setContent("文本消息已成功接收，内容为： " + inTextMsg.getContent());
+			render(outMsg);
+		}
+	}
+	
+	/**
+	 * 实现父类抽方法，处理图片消息
+	 */
+	protected void processInImageMsg(InImageMsg inImageMsg) {
+		OutTextMsg outMsg = new OutTextMsg(inImageMsg);
+		outMsg.setContent("processInImageMsg() 方法测试成功");
 		render(outMsg);
 	}
 	
-	protected void processInImageMessage(InImageMessage inImageMessage) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inImageMessage);
-		outMsg.setContent("processInImageMessage() 方法测试成功");
+	/**
+	 * 实现父类抽方法，处理语音消息
+	 */
+	protected void processInVoiceMsg(InVoiceMsg inVoiceMsg) {
+		OutVoiceMsg outMsg = new OutVoiceMsg(inVoiceMsg);
+		// 将刚发过来的语音再发回去
+		outMsg.setMediaId(inVoiceMsg.getMediaId());
 		render(outMsg);
 	}
 	
-	protected void processInVoiceMessage(InVoiceMessage inVoiceMessage) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inVoiceMessage);
-		outMsg.setContent("processInVoiceMessage() 方法测试成功");
+	/**
+	 * 实现父类抽方法，处理视频消息
+	 */
+	protected void processInVideoMsg(InVideoMsg inVideoMsg) {
+		OutVideoMsg outMsg = new OutVideoMsg(inVideoMsg);
+		outMsg.setTitle("jfinal weixin sdk 测试");
+		outMsg.setDescription("hahaha");
+		// 将刚发过来的视频再发回去
+		outMsg.setMediaId(inVideoMsg.getMediaId());
 		render(outMsg);
 	}
 	
-	protected void processInVideoMessage(InVideoMessage inVideoMessage) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inVideoMessage);
-		outMsg.setContent("processInVideoMessage() 方法测试成功");
+	/**
+	 * 实现父类抽方法，处理地址位置消息
+	 */
+	protected void processInLocationMsg(InLocationMsg inLocationMsg) {
+		OutTextMsg outMsg = new OutTextMsg(inLocationMsg);
+		outMsg.setContent("processInLocationMsg() 方法测试成功");
 		render(outMsg);
 	}
 	
-	protected void processInLocationMessage(InLocationMessage inLocationMessage) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inLocationMessage);
-		outMsg.setContent("processInLocationMessage() 方法测试成功");
+	/**
+	 * 实现父类抽方法，处理链接消息
+	 */
+	protected void processInLinkMsg(InLinkMsg inLinkMsg) {
+		OutTextMsg outMsg = new OutTextMsg(inLinkMsg);
+		outMsg.setContent("processInLinkMsg() 方法测试成功");
 		render(outMsg);
 	}
 	
-	protected void processInLinkMessage(InLinkMessage inLinkMessage) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inLinkMessage);
-		outMsg.setContent("processInLinkMessage() 方法测试成功");
-		render(outMsg);
-	}
-	
+	/**
+	 * 实现父类抽方法，处理关注/取消关注消息
+	 */
 	protected void processInFollowEvent(InFollowEvent inFollowEvent) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inFollowEvent);
+		OutTextMsg outMsg = new OutTextMsg(inFollowEvent);
 		outMsg.setContent("processInFollowEvent() 方法测试成功");
+		// 如果为取消关注事件，将无法接收到传回的信息
 		render(outMsg);
 	}
 	
+	/**
+	 * 实现父类抽方法，处理扫描带参数二维码事件
+	 */
 	protected void processInQrCodeEvent(InQrCodeEvent inQrCodeEvent) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inQrCodeEvent);
+		OutTextMsg outMsg = new OutTextMsg(inQrCodeEvent);
 		outMsg.setContent("processInQrCodeEvent() 方法测试成功");
 		render(outMsg);
 	}
 	
+	/**
+	 * 实现父类抽方法，处理上报地理位置事件
+	 */
 	protected void processInLocationEvent(InLocationEvent inLocationEvent) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inLocationEvent);
+		OutTextMsg outMsg = new OutTextMsg(inLocationEvent);
 		outMsg.setContent("processInLocationEvent() 方法测试成功");
 		render(outMsg);
 	}
 	
+	/**
+	 * 实现父类抽方法，处理自定义菜单事件
+	 */
 	protected void processInMenuEvent(InMenuEvent inMenuEvent) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inMenuEvent);
+		OutTextMsg outMsg = new OutTextMsg(inMenuEvent);
 		outMsg.setContent("processInMenuEvent() 方法测试成功");
 		render(outMsg);
 	}
 	
+	/**
+	 * 实现父类抽方法，处理接收语音识别结果
+	 */
 	protected void processInSpeechRecognitionResults(InSpeechRecognitionResults inSpeechRecognitionResults) {
-		OutTextMessage outMsg = new OutTextMessage();
-		initTestOutTextMessage(outMsg, inSpeechRecognitionResults);
+		OutTextMsg outMsg = new OutTextMsg(inSpeechRecognitionResults);
 		outMsg.setContent("processInSpeechRecognitionResults() 方法测试成功");
 		render(outMsg);
 	}
