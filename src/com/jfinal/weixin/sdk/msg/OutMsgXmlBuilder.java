11dc5582be7cb4d77e32701057bd6f9abe1d96f5
James <jfinal@126.com>
2014-09-27 22:42:57 +0800

    jfinal weixin sdk alpha version commit

--- src/com/jfinal/weixin/sdk/msg/OutMsgXmlBuilder.java
+++ src/com/jfinal/weixin/sdk/msg/OutMsgXmlBuilder.java
@@ -8,6 +8,7 @@ package com.jfinal.weixin.sdk.msg;
 
 import java.io.StringWriter;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import com.jfinal.weixin.sdk.msg.out.OutImageMsg;
 import com.jfinal.weixin.sdk.msg.out.OutMsg;
@@ -18,7 +19,9 @@ import com.jfinal.weixin.sdk.msg.out.OutVideoMsg;
 import com.jfinal.weixin.sdk.msg.out.OutVoiceMsg;
 import freemarker.cache.StringTemplateLoader;
 import freemarker.template.Configuration;
+import freemarker.template.ObjectWrapper;
 import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
 
 /**
  * 利用 FreeMarker 动态生成 OutMsg xml 内容 
@@ -54,6 +57,32 @@ public class OutMsgXmlBuilder {
 		StringTemplateLoader stringTemplateLoader = new StringTemplateLoader();
 		initStringTemplateLoader(stringTemplateLoader);
 		config.setTemplateLoader(stringTemplateLoader);
+		
+		// 模板缓存更新时间，对于OutMsg xml 在类文件中的模板来说已有热加载保障了更新
+        config.setTemplateUpdateDelay(999999);
+        // - Set an error handler that prints errors so they are readable with
+        //   a HTML browser.
+        // config.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+        config.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        
+        // - Use beans wrapper (recommmended for most applications)
+        config.setObjectWrapper(ObjectWrapper.BEANS_WRAPPER);
+        // - Set the default charset of the template files
+        config.setDefaultEncoding(encoding);		// config.setDefaultEncoding("ISO-8859-1");
+        // - Set the charset of the output. This is actually just a hint, that
+        //   templates may require for URL encoding and for generating META element
+        //   that uses http-equiv="Content-type".
+        config.setOutputEncoding(encoding);			// config.setOutputEncoding("UTF-8");
+        // - Set the default locale
+        config.setLocale(Locale.getDefault() /* Locale.CHINA */ );		// config.setLocale(Locale.US);
+        config.setLocalizedLookup(false);
+        
+        // 去掉int型输出时的逗号, 例如: 123,456
+        // config.setNumberFormat("#");		// config.setNumberFormat("0"); 也可以
+        config.setNumberFormat("#0.#####");
+        config.setDateFormat("yyyy-MM-dd");
+        config.setTimeFormat("HH:mm:ss");
+        config.setDateTimeFormat("yyyy-MM-dd HH:mm:ss");
 		return config;
 	}
 	
@@ -90,5 +119,9 @@ public class OutMsgXmlBuilder {
 		System.out.println(xml);
 	}
 }
+
+
+
+
 
 
