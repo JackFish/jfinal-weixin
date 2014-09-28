268cfd6ecd3416deb9c2f268a9efe95e9a1ab265
James <jfinal@126.com>
2014-09-28 11:15:40 +0800

    jfinal weixin sdk alpha version commit

--- src/com/jfinal/weixin/demo/DemoController.java
+++ src/com/jfinal/weixin/demo/DemoController.java
@@ -18,10 +18,10 @@ import com.jfinal.weixin.sdk.msg.in.event.InLocationEvent;
 import com.jfinal.weixin.sdk.msg.in.event.InMenuEvent;
 import com.jfinal.weixin.sdk.msg.in.event.InQrCodeEvent;
 import com.jfinal.weixin.sdk.msg.in.speech_recognition.InSpeechRecognitionResults;
+import com.jfinal.weixin.sdk.msg.out.OutImageMsg;
 import com.jfinal.weixin.sdk.msg.out.OutMusicMsg;
 import com.jfinal.weixin.sdk.msg.out.OutNewsMsg;
 import com.jfinal.weixin.sdk.msg.out.OutTextMsg;
-import com.jfinal.weixin.sdk.msg.out.OutVideoMsg;
 import com.jfinal.weixin.sdk.msg.out.OutVoiceMsg;
 
 /**
@@ -32,6 +32,8 @@ import com.jfinal.weixin.sdk.msg.out.OutVoiceMsg;
  */
 public class DemoController extends WeixinController {
 	
+	private static final String helpStr = "\t发送 help 可获得帮助，发送 \"美女\" 可看美女，发送 news 可看新闻，发送 music 可听音乐，你还可以试试发送图片、语音、位置、收藏等信息，看会有什么 。公众号持续更新中，想要更多惊喜欢迎每天关注 ^_^";
+	
 	/**
 	 * 实现父类抽方法，处理文本消息
 	 * 本例子中根据消息中的不同文本内容分别做出了不同的响应，同时也是为了测试 jfinal weixin sdk的基本功能：
@@ -41,7 +43,6 @@ public class DemoController extends WeixinController {
 	protected void processInTextMsg(InTextMsg inTextMsg) {
 		String msgContent = inTextMsg.getContent().trim();
 		// 帮助提示
-		String helpStr = "\t发送 help 可获得帮助，发送 news 可收取图文消息，发送 music 可获取音乐消息";
 		if ("help".equalsIgnoreCase(msgContent)) {
 			OutTextMsg outMsg = new OutTextMsg(inTextMsg);
 			outMsg.setContent(helpStr);
@@ -64,6 +65,11 @@ public class DemoController extends WeixinController {
 			outMsg.setFuncFlag(true);
 			render(outMsg);
 		}
+		else if ("美女".equalsIgnoreCase(msgContent)) {
+			OutNewsMsg outMsg = new OutNewsMsg(inTextMsg);
+			outMsg.addNews("秀色可餐", "JFinal Weixin 极速开发就是这么爽，有木有 ^_^", "http://mmbiz.qpic.cn/mmbiz/zz3Q6WSrzq2GJLC60ECD7rE7n1cvKWRNFvOyib4KGdic3N5APUWf4ia3LLPxJrtyIYRx93aPNkDtib3ADvdaBXmZJg/0", "http://mp.weixin.qq.com/s?__biz=MjM5ODAwOTU3Mg==&mid=200987822&idx=1&sn=7eb2918275fb0fa7b520768854fb7b80#rd");
+			render(outMsg);
+		}
 		// 其它文本消息直接返回原值 + 帮助提示
 		else {
 			OutTextMsg outMsg = new OutTextMsg(inTextMsg);
@@ -76,8 +82,9 @@ public class DemoController extends WeixinController {
 	 * 实现父类抽方法，处理图片消息
 	 */
 	protected void processInImageMsg(InImageMsg inImageMsg) {
-		OutTextMsg outMsg = new OutTextMsg(inImageMsg);
-		outMsg.setContent("processInImageMsg() 方法测试成功");
+		OutImageMsg outMsg = new OutImageMsg(inImageMsg);
+		// 将刚发过来的图片再发回去
+		outMsg.setMediaId(inImageMsg.getMediaId());
 		render(outMsg);
 	}
 	
@@ -95,11 +102,16 @@ public class DemoController extends WeixinController {
 	 * 实现父类抽方法，处理视频消息
 	 */
 	protected void processInVideoMsg(InVideoMsg inVideoMsg) {
+		/* 腾讯 api 有 bug，无法回复视频消息，暂时回复文本消息代码测试
 		OutVideoMsg outMsg = new OutVideoMsg(inVideoMsg);
-		outMsg.setTitle("jfinal weixin sdk 测试");
-		outMsg.setDescription("hahaha");
-		// 将刚发过来的视频再发回去
+		outMsg.setTitle("OutVideoMsg 发送");
+		outMsg.setDescription("刚刚发来的视频再发回去");
+		// 将刚发过来的视频再发回去，经测试证明是腾讯官方的 api 有 bug，待 api bug 却除后再试
 		outMsg.setMediaId(inVideoMsg.getMediaId());
+		render(outMsg);
+		*/
+		OutTextMsg outMsg = new OutTextMsg(inVideoMsg);
+		outMsg.setContent("\t视频消息已成功接收，该视频的 mediaId 为: " + inVideoMsg.getMediaId());
 		render(outMsg);
 	}
 	
@@ -108,16 +120,21 @@ public class DemoController extends WeixinController {
 	 */
 	protected void processInLocationMsg(InLocationMsg inLocationMsg) {
 		OutTextMsg outMsg = new OutTextMsg(inLocationMsg);
-		outMsg.setContent("processInLocationMsg() 方法测试成功");
+		outMsg.setContent("已收到地理位置消息:" +
+							"\nlocation_X = " + inLocationMsg.getLocation_X() +
+							"\nlocation_Y = " + inLocationMsg.getLocation_Y() + 
+							"\nscale = " + inLocationMsg.getScale() +
+							"\nlabel = " + inLocationMsg.getLabel());
 		render(outMsg);
 	}
 	
 	/**
 	 * 实现父类抽方法，处理链接消息
+	 * 特别注意：测试时需要发送我的收藏中的曾经收藏过的图文消息，直接发送链接地址会当做文本消息来发送
 	 */
 	protected void processInLinkMsg(InLinkMsg inLinkMsg) {
-		OutTextMsg outMsg = new OutTextMsg(inLinkMsg);
-		outMsg.setContent("processInLinkMsg() 方法测试成功");
+		OutNewsMsg outMsg = new OutNewsMsg(inLinkMsg);
+		outMsg.addNews("链接消息已成功接收", "链接使用图文消息的方式发回给你，还可以使用文本方式发回。点击图文消息可跳转到链接地址页面，是不是很好玩 :)" , "http://mmbiz.qpic.cn/mmbiz/zz3Q6WSrzq1ibBkhSA1BibMuMxLuHIvUfiaGsK7CC4kIzeh178IYSHbYQ5eg9tVxgEcbegAu22Qhwgl5IhZFWWXUw/0", inLinkMsg.getUrl());
 		render(outMsg);
 	}
 	
@@ -126,7 +143,7 @@ public class DemoController extends WeixinController {
 	 */
 	protected void processInFollowEvent(InFollowEvent inFollowEvent) {
 		OutTextMsg outMsg = new OutTextMsg(inFollowEvent);
-		outMsg.setContent("processInFollowEvent() 方法测试成功");
+		outMsg.setContent("感谢关注 JFinal Weixin 极速开发，为您节约更多时间，去陪恋人、家人和朋友 :) \n\n\n " + helpStr);
 		// 如果为取消关注事件，将无法接收到传回的信息
 		render(outMsg);
 	}
