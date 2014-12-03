e80ad5d816bc7a3301f8f87294f063d1b253dc52
James <jfinal@126.com>
2014-12-03 23:14:47 +0800

    jfinal weixin 1.1 release ^_^

--- src/com/jfinal/weixin/sdk/kit/MsgEncryptKit.java
+++ src/com/jfinal/weixin/sdk/kit/MsgEncryptKit.java
@@ -13,6 +13,14 @@ import com.jfinal.weixin.sdk.encrypt.WXBizMsgCrypt;
 /**
  * 对微信平台官方给出的加密解析代码进行再次封装
  * 
+ * 异常java.security.InvalidKeyException:illegal Key Size的解决方案：
+ * 1：在官方网站下载JCE无限制权限策略文件（JDK7的下载地址：
+ *     http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html
+ * 2：下载后解压，可以看到local_policy.jar和US_export_policy.jar以及readme.txt
+ * 3：如果安装了JRE，将两个jar文件放到%JRE_HOME%\lib\security目录下覆盖原来的文件
+ * 4：如果安装了JDK，将两个jar文件放到%JDK_HOME%\jre\lib\security目录下覆盖原来文件
+ * 
+ * 
  * 设置为消息加密模式后 JFinal Action Report 中有如下参数：
  * timestamp=1417610658
  * encrypt_type=aes
