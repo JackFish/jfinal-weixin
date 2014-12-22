04ad5f362b880e6aaec8ac46973485884da40e28
James <jfinal@126.com>
2014-12-22 22:56:05 +0800

    jfinal weixin 1.3 release ^_^

--- src/com/jfinal/weixin/demo/WeixinConfig.java
+++ src/com/jfinal/weixin/demo/WeixinConfig.java
@@ -6,7 +6,6 @@
 
 package com.jfinal.weixin.demo;
 
-import java.util.Properties;
 import com.jfinal.config.Constants;
 import com.jfinal.config.Handlers;
 import com.jfinal.config.Interceptors;
@@ -14,21 +13,29 @@ import com.jfinal.config.JFinalConfig;
 import com.jfinal.config.Plugins;
 import com.jfinal.config.Routes;
 import com.jfinal.core.JFinal;
+import com.jfinal.kit.PropKit;
 import com.jfinal.plugin.ehcache.EhCachePlugin;
 import com.jfinal.weixin.sdk.api.ApiConfigKit;
 
 public class WeixinConfig extends JFinalConfig {
 	
-	public Properties loadProp(String pro, String dev) {
-		try {return loadPropertyFile(pro);}
-		catch (Exception e)
-			{return loadPropertyFile(dev);}
+	/**
+	 * 如果生产环境配置文件存在，则优先加载该配置，否则加载开发环境配置文件
+	 * @param pro 生产环境配置文件
+	 * @param dev 开发环境配置文件
+	 */
+	public void loadProp(String pro, String dev) {
+		try {
+			PropKit.use(pro);
+		}
+		catch (Exception e) {
+			PropKit.use(dev);
+		}
 	}
 	
 	public void configConstant(Constants me) {
-		// 如果生产环境配置文件存在，则优先加载该配置，否则加载开发环境配置文件
 		loadProp("a_little_config_pro.txt", "a_little_config.txt");
-		me.setDevMode(getPropertyToBoolean("devMode", false));
+		me.setDevMode(PropKit.getBoolean("devMode", false));
 		
 		// ApiConfigKit 设为开发模式可以在开发阶段输出请求交互的 xml 与 json 数据
 		ApiConfigKit.setDevMode(me.getDevMode());
@@ -40,7 +47,7 @@ public class WeixinConfig extends JFinalConfig {
 	}
 	
 	public void configPlugin(Plugins me) {
-		// C3p0Plugin c3p0Plugin = new C3p0Plugin(getProperty("jdbcUrl"), getProperty("user"), getProperty("password").trim());
+		// C3p0Plugin c3p0Plugin = new C3p0Plugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password").trim());
 		// me.add(c3p0Plugin);
 		
 		EhCachePlugin ecp = new EhCachePlugin();
