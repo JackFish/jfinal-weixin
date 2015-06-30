454793225a1a79740499ccd8b0717878c66bd00a
unas <unas@qq.com>
2015-06-30 19:44:11 +0800

    修改方法名

--- src/com/jfinal/weixin/sdk/api/TemplateMsgApi.java
+++ src/com/jfinal/weixin/sdk/api/TemplateMsgApi.java
@@ -13,13 +13,13 @@ import com.jfinal.kit.HttpKit;
  */
 public class TemplateMsgApi {
 	
-	private static String sendTemplateMsg = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=";
+	private static String sendApiUrl = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=";
 	
 	/**
 	 * 发送模板消息
 	 */
-	public static ApiResult sendTemplateMsg(String jsonStr) {
-		String jsonResult = HttpKit.post(sendTemplateMsg + AccessTokenApi.getAccessToken().getAccessToken(), jsonStr);
+	public static ApiResult send(String jsonStr) {
+		String jsonResult = HttpKit.post(sendApiUrl + AccessTokenApi.getAccessToken().getAccessToken(), jsonStr);
 		return new ApiResult(jsonResult);
 	}
 }
