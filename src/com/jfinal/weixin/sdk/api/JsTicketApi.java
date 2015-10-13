f3905a114f4b636c5a2600874a78fda983713b40
孤独的√3 <596392912@qq.com>
2015-10-13 22:42:54 +0800

    fix

--- src/com/jfinal/weixin/sdk/api/JsTicketApi.java
+++ src/com/jfinal/weixin/sdk/api/JsTicketApi.java
@@ -45,7 +45,8 @@ public class JsTicketApi {
 	 */
 	public static JsTicket getTicket(JsApiType jsApiType) {
 		String access_token = AccessTokenApi.getAccessToken().getAccessToken();
-		String key = access_token + ':' + jsApiType.name();
+		String appId = ApiConfigKit.getApiConfig().getAppId();
+		String key = appId + ':' + jsApiType.name();
 
 		JsTicket jsTicket = accessTokenCache.get(key);
 		if (null == jsTicket || !jsTicket.isAvailable()) {
