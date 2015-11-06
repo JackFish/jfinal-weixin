9a0d4e383411956f40df25f555b117c843c8616a
孤独的√3 <596392912@qq.com>
2015-11-06 18:42:53 +0800

    add

---
+++ src/com/jfinal/weixin/sdk/api/MediaApi.java
@@ -0,0 +1,11 @@
+package com.jfinal.weixin.sdk.api;
+
+/**
+ * 素材管理
+ * @author l.cm
+ * 文档：http://mp.weixin.qq.com/wiki/5/963fc70b80dc75483a271298a76a8d59.html
+ */
+public class MediaApi {
+
+	
+}
