a3c3356253164df5b61f2345ceda365e3021665d
zj_ren8@163.com <zj_ren8@163.com>
2015-11-17 18:02:44 +0800

    add LBS api

--- src/com/jfinal/weixin/sdk/msg/in/event/InLocationEvent.java
+++ src/com/jfinal/weixin/sdk/msg/in/event/InLocationEvent.java
@@ -1,57 +1,63 @@
 /**
  * Copyright (c) 2011-2014, James Zhan 詹波 (jfinal@126.com).
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  */
 
 package com.jfinal.weixin.sdk.msg.in.event;
 
 /**
-	上报地理位置事件
-	<xml>
-		<ToUserName><![CDATA[toUser]]></ToUserName>
-		<FromUserName><![CDATA[fromUser]]></FromUserName>
-		<CreateTime>123456789</CreateTime>
-		<MsgType><![CDATA[event]]></MsgType>
-			<Event><![CDATA[LOCATION]]></Event>
-			<Latitude>23.137466</Latitude>
-			<Longitude>113.352425</Longitude>
-			<Precision>119.385040</Precision>
-	</xml>
+ * 上报地理位置事件
+ * <xml>
+ * <ToUserName><![CDATA[toUser]]></ToUserName>
+ * <FromUserName><![CDATA[fromUser]]></FromUserName>
+ * <CreateTime>123456789</CreateTime>
+ * <MsgType><![CDATA[event]]></MsgType>
+ * <Event><![CDATA[LOCATION]]></Event>
+ * <Latitude>23.137466</Latitude>
+ * <Longitude>113.352425</Longitude>
+ * <Precision>119.385040</Precision>
+ * </xml>
  */
 public class InLocationEvent extends EventInMsg {
-	
-	private String latitude;
-	private String longitude;
-	private String precision;
-	
-	public InLocationEvent(String toUserName, String fromUserName, Integer createTime, String msgType, String event) {
-		super(toUserName, fromUserName, createTime, msgType, event);
-	}
 
-	public String getLatitude() {
-		return latitude;
-	}
-	
-	public void setLatitude(String latitude) {
-		this.latitude = latitude;
-	}
-	
-	public String getLongitude() {
-		return longitude;
-	}
-	
-	public void setLongitude(String longitude) {
-		this.longitude = longitude;
-	}
-	
-	public String getPrecision() {
-		return precision;
-	}
-	
-	public void setPrecision(String precision) {
-		this.precision = precision;
-	}
+    private String latitude;
+    private String longitude;
+    private String precision;
+
+    public InLocationEvent(String toUserName, String fromUserName, Integer createTime, String msgType, String event) {
+        super(toUserName, fromUserName, createTime, msgType, event);
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getPrecision() {
+        return precision;
+    }
+
+    public void setPrecision(String precision) {
+        this.precision = precision;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        InLocationEvent other = (InLocationEvent) obj;
+        return this.precision.equals(other.getPrecision()) && this.getLatitude().equals(other.getLatitude()) && this.getLongitude().equals(other.getLongitude());
+    }
 }
 
 
